dc89e238243d528d71a8cc3f2d74bb6e5c48442e
Rik Oostveen <rik@askwadraat.nl>
2026-01-08 11:25:47 +0100

    Add saved results functionality for daily quizzes, streak tracking, and enhanced answer reviews.

--- Models/DailyResult.cs
+++ Models/DailyResult.cs
@@ -6,4 +6,21 @@ public class DailyResult
     public int Score { get; set; }
     public int TotalQuestions { get; set; }
     public List<string> Categories { get; set; } = [];
+    public List<SavedAnswer> Answers { get; set; } = [];
+}
+
+public class SavedAnswer
+{
+    public string ItemName { get; set; } = "";
+    public List<string> CorrectCategories { get; set; } = [];
+    public List<string> SelectedCategories { get; set; } = [];
+    public bool WasCorrect { get; set; }
+    public string Explanation { get; set; } = "";
+}
+
+public class DailyHistoryEntry
+{
+    public string Date { get; set; } = "";
+    public int Score { get; set; }
+    public int TotalQuestions { get; set; }
 }
